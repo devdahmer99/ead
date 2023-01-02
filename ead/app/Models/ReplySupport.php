1f82bab5a96f348860f6643acc00e7e944412f69
Eduardo Dahmer <eduardodahmer99@gmail.com>
2023-01-01 23:01:47 -0300

    Implementação dos endpoins de duvidas

---
+++ ead/app/Models/ReplySupport.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReplySupport extends Model
+{
+    use HasFactory, UuidTrait;
+
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+    protected $fillable = ['description', 'support_id'];
+    protected $table = ['reply_support'];
+
+    public function support()
+    {
+        return $this->belongsTo(Support::class);
+    }
+
+    public function user()
+    {
+        return $this->hasMany(User::class);
+    }
+
+}

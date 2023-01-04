efd7babd2ca1c7f13036b2f629a9ed63bafdbc7f
Eduardo Dahmer <eduardodahmer99@gmail.com>
2023-01-04 09:53:47 -0300

    Implementação do Middleware de autenticação das rotas

--- routes/api.php
+++ routes/api.php
@@ -9,18 +9,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth', [AuthController::class, 'auth']);
 
-Route::get('/courses',[CourseController::class, 'index']);
-Route::get('/courses/{id}',[CourseController::class, 'show']);
-Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
-Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
-Route::get('/lessons/{id}', [LessonController::class, 'show']);
-
-
-Route::get('/supports', [SupportController::class, 'index']);
-Route::post('/supports', [SupportController::class, 'store']);
-
-Route::post('/supports/{id}/replies', [SupportController::class, 'createReply']);
-
+Route::middleware(['auth:sactum'])->group(function() {
+    Route::get('/courses',[CourseController::class, 'index']);
+    Route::get('/courses/{id}',[CourseController::class, 'show']);
+    Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+    Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+    Route::get('/lessons/{id}', [LessonController::class, 'show']);
+    Route::get('/supports', [SupportController::class, 'index']);
+    Route::post('/supports', [SupportController::class, 'store']);
+    Route::post('/supports/{id}/replies', [SupportController::class, 'createReply']);
+});
 
 
 Route::get('/', function () {
